31db0d8b37757d8425bcc5e070f3b6f16261f693
Julian Sandoval <76192811+Julian-Sandoval-x@users.noreply.github.com>
2024-12-05 07:45:56 -0800

    Correccion de servidor

--- src/js/server.js
+++ src/js/server.js
@@ -1,16 +1,24 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 const { static: serveStatic } = express;
 const app = express();
 const port = process.env.PORT || 8080; // Utiliza el puerto de Railway o el puerto 8080 por defecto
 
-app.use(serveStatic("public"));
+// Obtener la ruta del directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../.."))); // '..' sube un nivel a la raíz
 
 // Ruta de ejemplo
 app.get("/", (req, res) => {
-  res.redirect("build/index.html");
+  res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
 // Escucha en el puerto definido por Railway o en el 8080 por defecto
 app.listen(port, () => {
-  console.log(`Servidor escuchando en el puerto ${port}`);
+  console.log(
+    `Servidor escuchando en el puerto ${port}, dir  ../../${__dirname}`
+  );
 });
