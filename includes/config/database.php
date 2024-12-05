8f9e8096c8b625a481bc77d411730d22a8c081ef
Julian Sandoval <76192811+Julian-Sandoval-x@users.noreply.github.com>
2024-12-05 06:59:00 -0800

    Modificacion de credenciales deBD

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
 
 function conectarDB(): mysqli
 {
-    $db = new mysqli('localhost', 'root', 'logiPruebas1.', 'noma');
+    $db = new mysqli('mysql.railway.internal', 'root', 'logiPruebas1.', 'noma');
 
     if (!$db) {
         echo "Error no se pudo conectar";
